ce577ed8b04dfe20d80a9c79100aace009117744
kllerqueen <102903801+kllerqueen@users.noreply.github.com>
2023-11-20 23:42:24 +0700

    final push

--- src/App.js
+++ src/App.js
@@ -1,14 +1,109 @@
 import "./styles.css";
+import { useState } from "react";
 
-export default function ListingAd({ pic, title, address, description }) {
+export default function ListingAd({
+  title,
+  address,
+  description,
+  descriptionMiddle,
+  descriptionMiddle2,
+  descriptionMiddle3,
+  descriptionEnd,
+  clickable_num1,
+  clickable_num2,
+  clickable_num3,
+  project_type,
+  year,
+  ownership_type,
+  availabilities_label,
+  psf_min,
+  psf_max,
+  subprice_label
+}) {
+  const [desc, setDesc] = useState(false);
+  const [num, setNum] = useState(false);
+  const handleClick = () => setDesc(!desc);
+  const handleClickNum = () => setNum(!num);
+
   return (
     <div className="App">
-      <img className="mainPic" width="300" height="500" src={pic} />
-      <div className="mainContent">
-        <h1>{title}</h1>
-        <p className="address">{address}</p>
-        <button>See description</button>
-        <p className="description">{description}</p>
+      <div className="cardContainer">
+        <div className="imageWrapper">
+          <div className="temp">
+            <img className="mainPic" src="/Building.webp" />
+            <img className="flag" src="/Flag.webp" alt="/" />
+          </div>
+
+          <div className="arrows">
+            <p className="arrow">{"<"}</p>
+            <p className="arrow">{">"}</p>
+          </div>
+        </div>
+
+        <div className="wrapper">
+          <div className="grid">
+            <div>
+              <div className="buildingInfo">
+                <img src="/building-icon.svg" alt="/" />
+                <div>
+                  <h1 className="title">{title}</h1>
+                  <span className="subTitle">{address}</span>
+                </div>
+              </div>
+              <div className="additionalInfo">
+                <p className="typeInfo">
+                  {project_type} · {year} · {ownership_type}
+                </p>
+                <p className="typeInfo">{availabilities_label}</p>
+              </div>
+            </div>
+
+            <div className="buildingPrice">
+              <h1 className="title">
+                ${psf_min} - ${psf_max} psf
+              </h1>
+              <span className="subTitle">{subprice_label}</span>
+            </div>
+          </div>
+
+          <div className="buttonContainer">
+            <button className="descButton" onClick={handleClick}>
+              See description
+            </button>
+          </div>
+
+          <p className={`descText ${desc ? "fade-in" : "fade-out"}`}>
+            {!desc ? (
+              ""
+            ) : (
+              <>
+                {description}{" "}
+                <span className="phoneNum" onClick={handleClickNum}>
+                  {!num
+                    ? clickable_num1.substring(0, 4) + " XXXX"
+                    : clickable_num1}
+                </span>{" "}
+                <br />
+                <br />
+                {descriptionMiddle} <br />
+                <br />
+                {descriptionMiddle2}
+                <span className="phoneNum" onClick={handleClickNum}>
+                  {!num
+                    ? clickable_num2.substring(0, 4) + "XXXX"
+                    : clickable_num2}
+                </span>{" "}
+                {descriptionMiddle3}
+                <span className="phoneNum" onClick={handleClickNum}>
+                  {!num
+                    ? clickable_num3.substring(0, 5) + "XXXX)"
+                    : clickable_num3}
+                </span>{" "}
+                {descriptionEnd}
+              </>
+            )}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 
 const rootElement = document.getElementById("root");
 const store = {
-  pic: "https://images.prismic.io/99-content/dc1594fb-f413-44ff-b8b5-c3ba6cd539cd_vbWkNuxt2hjgidVttoMRDX.jpg?auto=compress,format&w=2000&q=100",
   title: "Forett At Bukit Timah",
   address: "70 Anchorvale Crescent · D21",
   project_type: "Condo",
@@ -15,12 +14,15 @@ const store = {
   psf_max: 3043,
   subprice_label: "Prices from $1.99M",
   availabilities_label: "231 units · 1, 2, 3, 4 bedrooms",
-  description: `Phone number: 8234 5678
-
-Superb 3 bedroom in sentosa for a very affordable price and a very nearby bus
-station.
-
-Please call Alex B (CEA No: R0123456) @  82345678 or the owner (83456789) to arrange for a viewing.`,
+  description: "Phone number:",
+  clickable_num1: "8234 5678",
+  descriptionMiddle:
+    "Superb 3 bedroom in sentosa for a very affordable price and a very nearby bus station.",
+  descriptionMiddle2: "Please call Alex B (CEA No: R0123456) @",
+  clickable_num2: "82345678",
+  descriptionMiddle3: "or the owner",
+  clickable_num3: "(83456789)",
+  descriptionEnd: "to arrange for a viewing."
 };
 
 ReactDOM.render(
